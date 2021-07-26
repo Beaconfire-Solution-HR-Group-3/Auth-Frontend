8c0a29cce296b1fd1133f9b9c1ccef8fd412c43c
YuhanChen8789 <YuhanChen8789@outlook.com>
2021-07-27 04:16:12 +0800

    add redirect function

--- Auth-Frontend/src/app/login/login.component.ts
+++ Auth-Frontend/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,22 +12,41 @@ export class LoginComponent implements OnInit {
 
   userName: string;
   password: string;
+  redirect:string;
+  failed:boolean = false;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient, private route: ActivatedRoute) { 
     this.userName = "";
     this.password = "";
+    this.redirect = "";
+
 
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.redirect = params['redirect'];
+      console.log(this.redirect);
+    });
   }
   onSubmit() {
     let headers = new HttpHeaders({
-      "Allow-Cross-Origin-Origin0": "*" });
+      "Access-Control-Allow-Origin": "*" });
     let options = { headers: headers };
 
     this.http.post<any>('http://localhost:9999/login', 
-    {"userName": this.userName, "password": this.password, "redirect": ""}, options).subscribe(data => 
-    console.log(data));
+    {"userName": this.userName, "password": this.password, "redirect": this.redirect}, options).subscribe(data => 
+      {
+        if(data.status.success == true){
+          window.location.href = this.redirect;
+        }
+        else{
+          this.failed = true;
+        }
+        
+       
+    });
+      
+   
   }
 }
